990efa43781c776cff62db94f026e447b05f62f2
Roman Daniel <romandaniel18@stud.ase.ro>
2020-11-01 20:16:26 +0200

    Tema 1 Roman Daniel

--- main/app.js
+++ main/app.js
@@ -1,9 +1,37 @@
 function removeOrderItem(orderInfo, position){
+     
+    if(orderInfo.items.constructor===Array){}
+    else{throw "Items should be an array";} 
     
-}
-
-const app = {
-    removeOrderItem
-};
-
-module.exports = app;
+    for(var i=0;i<orderInfo.items.length;i++){
+    if(orderInfo.items[i].quantity && orderInfo.items[i].price){}
+    else{throw"Malformed item";}
+    }
+    
+    /*orderInfo.items.forEach( element=> {
+    if( !element.hasOwnProperty('price') || !element.hasOwnProperty('quantity')){
+     throw"Malformed item";
+    }
+    }); */
+    if(position >0 && position <= orderInfo.items.length){} 
+    else {throw "Invalid position";} 
+    
+    /*if(orderInfo.items['price'] !== undefined && orderInfo.items['quantity'] !== undefined){;} 
+    else { return orderInfo;} */
+    
+    orderInfo.items.splice(position,1);
+    orderInfo.total=0;
+    for(var i=0;i<orderInfo.items.length;i++){
+        orderInfo.total+= orderInfo.items[i].quantity * orderInfo.items[i].price;
+    }
+    return orderInfo;
+    
+    
+    }
+    
+    const app = {
+        removeOrderItem
+    };
+    
+    module.exports = app;
+    
